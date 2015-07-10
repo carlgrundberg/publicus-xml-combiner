6881e191ac62a44b17bea4d9a260c47df7a106ce
Carl Grundberg <carl.grundberg@menmo.se>
2015-07-10 16:11:53 +0200

    Support for multiple taxonomies per site

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ var dir = 'e:/pub/';
 var sites = [
     {
         name: 'skd',
-        tax: 'Skånska Dagbladet',
+        tax: ['Skånska Dagbladet', 'Lund', 'Malmö', 'Burlöv', 'Eslöv', 'Hörby', 'Kävlinge', 'Landskrona', 'Lomma', 'Nordost', 'Sjöbo', 'Svedala', 'Svalöv', 'Trelleborg', 'Vellinge', 'Ystad', 'Höör', 'Arlöv', 'Åkarp', 'Helsingborg', 'Bjuv', 'Klippan', 'Ängelholm', 'Örkelljunga', 'Åstorp', 'Dalby', 'Genarp', 'Södra Sandby', 'Veberöd', 'Blentarp', 'Veberöd', 'Vollsjö', 'Kivik', 'Simrishamn', 'Tomelilla'],
         out: fs.createWriteStream('skd.xml'),
         count: 0
     },
     {
         name: 'nsk',
-        tax: 'Norra Skåne',
+        tax: ['Norra Skåne', 'Hässleholm', 'Bjärnum', 'Hästveda', 'Tyringe', 'Vankiva', 'Vinslöv', 'Vittsjö', 'Kristianstad', 'Arkelstorp', 'Bromölla', 'Tollarp', 'Åhus', 'Markaryd', 'Osby', 'Perstorp', 'Östra Göinge', 'Broby', 'Hanaskog', 'Knislinge'],
         out: fs.createWriteStream('nsk.xml'),
         count: 0
     },
     {
         name: 'lt',
-        tax: 'Laholms Tidning',
+        tax: ['Laholms Tidning', 'Laholm', 'Borås', 'Båstad', 'Halmstad'],
         out: fs.createWriteStream('lt.xml'),
         count: 0
     }
@@ -42,20 +42,23 @@ fs.readdir(dir, function (err, files) {
 
     sites.map(fileheader);
 
-    for(var i = 0; i < files.length; i++) {
+    for (var i = 0; i < files.length; i++) {
         var file = files[i];
         if (path.extname(file) === ".xml") {
             console.log(file);
             var input = iconv.decode(fs.readFileSync(dir + file), 'win1252');
             input.replace('<?xml version="1.0" encoding="windows-1252"?>', '');
             var tax = input.match(/<taxonomytext><\!\[CDATA\[(.*)\]\]><\/taxonomytext>/);
-            if(tax) {
-                for(var j = 0; j < sites.length; j++) {
+            if (tax) {
+                for (var j = 0; j < sites.length; j++) {
                     var site = sites[j];
-                    if(tax[1].indexOf(site.tax) != -1) {
-                        console.log('Appending to ' + site.name);
-                        site.out.write(input);
-                        site.count++;
+                    for (var k = 0; k < site.tax.length; k++) {
+                        if (tax[1].indexOf(site.tax[k]) != -1) {
+                            console.log('Appending to ' + site.name);
+                            site.out.write(input);
+                            site.count++;
+                            break;
+                        }
                     }
                 }
             }
@@ -63,7 +66,7 @@ fs.readdir(dir, function (err, files) {
     }
 
     console.log('Number of articles appended for each site');
-    for(j = 0; j < sites.length; j++) {
+    for (j = 0; j < sites.length; j++) {
         site = sites[j];
         console.log(site.name + ':' + site.count);
     }
