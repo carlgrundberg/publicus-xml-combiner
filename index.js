dedd3f59e50d5e31a8fbd0f11a9286dee47ce593
Carl Grundberg <carl.grundberg@menmo.se>
2015-07-27 14:09:18 +0200

    outdir support

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 var iconv = require('iconv-lite');
 var fs = require('fs');
 var path = require("path");
-var dir = 'e:/pub/';
+var indir = 'F:/pubarchive/2014_Jan-July/';
+var outdir = 'F:/pubarchive_combined/2014_Jan-July/';
 
 var sites = [
     {
         name: 'skd',
         tax: ['Skånska Dagbladet', 'Lund', 'Malmö', 'Burlöv', 'Eslöv', 'Hörby', 'Kävlinge', 'Landskrona', 'Lomma', 'Nordost', 'Sjöbo', 'Svedala', 'Svalöv', 'Trelleborg', 'Vellinge', 'Ystad', 'Höör', 'Arlöv', 'Åkarp', 'Helsingborg', 'Bjuv', 'Klippan', 'Ängelholm', 'Örkelljunga', 'Åstorp', 'Dalby', 'Genarp', 'Södra Sandby', 'Veberöd', 'Blentarp', 'Veberöd', 'Vollsjö', 'Kivik', 'Simrishamn', 'Tomelilla'],
-        out: fs.createWriteStream('skd.xml'),
+        out: fs.createWriteStream(outdir + 'skd.xml'),
         count: 0
     },
     {
         name: 'nsk',
         tax: ['Norra Skåne', 'Hässleholm', 'Bjärnum', 'Hästveda', 'Tyringe', 'Vankiva', 'Vinslöv', 'Vittsjö', 'Kristianstad', 'Arkelstorp', 'Bromölla', 'Tollarp', 'Åhus', 'Markaryd', 'Osby', 'Perstorp', 'Östra Göinge', 'Broby', 'Hanaskog', 'Knislinge'],
-        out: fs.createWriteStream('nsk.xml'),
+        out: fs.createWriteStream(outdir + 'nsk.xml'),
         count: 0
     },
     {
         name: 'lt',
         tax: ['Laholms Tidning', 'Laholm', 'Borås', 'Båstad', 'Halmstad'],
-        out: fs.createWriteStream('lt.xml'),
+        out: fs.createWriteStream(outdir + 'lt.xml'),
         count: 0
     }
 ];
 
-console.log('Reading directory ' + dir);
+console.log('Reading directory ' + indir);
 
 function fileheader(site) {
     var out = site.out;
@@ -37,7 +38,7 @@ function filefooter(site) {
     out.write('</articles>\n');
 }
 
-fs.readdir(dir, function (err, files) {
+fs.readdir(indir, function (err, files) {
     if (err) throw err;
 
     sites.map(fileheader);
@@ -46,7 +47,7 @@ fs.readdir(dir, function (err, files) {
         var file = files[i];
         if (path.extname(file) === ".xml") {
             console.log(file);
-            var input = iconv.decode(fs.readFileSync(dir + file), 'win1252').replace('<?xml version="1.0" encoding="windows-1252"?>', '');
+            var input = iconv.decode(fs.readFileSync(indir + file), 'win1252').replace('<?xml version="1.0" encoding="windows-1252"?>', '');
             var tax = input.match(/<taxonomytext><\!\[CDATA\[(.*)\]\]><\/taxonomytext>/);
             if (tax) {
                 for (var j = 0; j < sites.length; j++) {
